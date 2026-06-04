b33935f167774c07259337bc0afe6a5a109c746e
AntoniRagaMocholi <antoniwintermono@gmail.com>
2026-06-04 15:41:54 +0200

    Update puenteMQTT.py

--- puenteMQTT.py
+++ puenteMQTT.py
@@ -1,99 +1,91 @@
+/*
 import paho.mqtt.client as mqtt
 import psycopg
 import json
 from datetime import datetime
 
-# --- Base de datos---
-
 DB_PARAMS = {
-    "dbname": "incubadora_db",
+    "dbname": "proyecto_pr2",
     "user": "postgres",
-    "password": "postgres", 
+    "password": "postgres",
     "host": "localhost",
     "port": "5432"
 }
 
-# --- Parámetros seguridad (ejemplo) ---
-
 CONFIG_CONTROL = {
     "esp1": {"co2_limite": 800, "o2_min": 19},
-    "esp2": {"temp_max": 38.5,}, 
+    "esp2": {"temp_max": 38.5, "distancia_min": 10.0},
     "esp3": {"vibe_max": 2.0}
 }
+#   Ejemplo mensaje recibido por el MQTT;
+#       {
+#       "temperatura": 37.2,
+#       "vibracion": 0.15,
+#       "o2": 20.5,
+#       "co2": 5.1,
+#       }
+#
+#       {
+#       "parada_emergencia": 1
+#       }
+#
+#       {
+#       "placa_procesada": 1
+#       }
 
-# MQTTX
 
 MQTT_USER = "giirob"
 MQTT_PASS = "UPV2024"
-MQTT_BROKER = "mqtt.dsic.upv.es" 
+MQTT_BROKER = "mqtt.dsic.upv.es"
 MQTT_PORT = 1883
 
 def guardar_en_db(topic, payload):
     try:
-        # Extraemos el ID del dispositivo (ej: "incubadora/esp2" -> "esp2")
+        # Extraemos el ID del dispositivo (ej: "nombre_laboratorio/cod_planta/cod_incubadora")
         partes_topic = topic.split('/')
-        dispositivo = partes_topic[1] 
+        cod_planta = int(partes_topic[1])
+        cod_incubadora = int(partes_topic[2])
         datos = json.loads(payload)
-        
+       
         with psycopg.connect(**DB_PARAMS) as conn:
             with conn.cursor() as cur:
-                
-                # El bucle FOR recorre cada mensaje JSON
 
                 for parametro, valor in datos.items():
-
+   
                     # Guardar datos lectura
-                    cur.execute(
-                        "INSERT INTO incubadora.lecturas_sensores (dispositivo, tipo_parametro, valor) VALUES (%s, %s, %s)",
-                        (dispositivo, parametro, valor)
-                    )
+                    #cur.execute(
+                        #"INSERT INTO incubadora.lecturas_sensores (dispositivo, tipo_parametro, valor) VALUES (%s, %s, %s)",
+                        #(dispositivo, parametro, valor)
+                    #)
+                    if parametro == "parada_emergencia" and valor == 1:
+                        cur.execute(
+                            "UPDATE Planta SET n_paradas = n_paradas + 1 WHERE codigo = %s;"
+                            (cod_planta,)
+                        )
+                    elif parametro == "placa_procesada" and valor == 1:
+                        cur.execute(
+                            "UPDATE Planta SET n_procesadas = n_procesadas + 1 WHERE codigo = %s;"
+                            (cod_planta,)
+                        )
+                    else:
 
-                    if dispositivo == "esp2" and parametro == "presencia" and valor == 1:
-                        cur.execute(
-                            "INSERT INTO incubadora.lecturas_sensores (dispositivo, tipo_parametro, valor) VALUES (%s, %s, %s)",
-                            (dispositivo, parametro, valor)
-                        )
-                        print(f"! PRESENCIA DE STACK !")
-                    
-                    # Boton emergencia
-                    elif dispositivo == "esp2" and parametro == "boton" and valor == 1:
-                        cur.execute(
-                            "INSERT INTO incubadora.incidencias (nivel_prioridad, descripcion, estado_robot) VALUES (%s, %s, %s)",
-                            ("CRÍTICO", "BOTÓN DE EMERGENCIA PULSADO", "STOP")
-                        )
-                        print("!!! EMERGENCIA: PARADA SOLICITADA !!!")
 
-                    # Co2 elevado
-                    elif dispositivo == "esp1" and parametro == "co2" and valor > CONFIG_CONTROL["esp1"]["co2_limite"]:
-                        cur.execute(
-                            "INSERT INTO incubadora.incidencias (nivel_prioridad, descripcion, estado_robot) VALUES (%s, %s, %s)",
-                            ("ADVERTENCIA", f"CO2 elevado: {valor}ppm", "OPERATIVO")
-                        )
+                        temp = datos.get("temperatura")
+                        vibracion = datos.get("vibracion")
+                        o2 = datos.get("o2")
+                        co2 = datos.get("co2")
 
-                    # Oxígeno bajo
-                    elif dispositivo == "esp1" and parametro == "o2" and valor < CONFIG_CONTROL["esp1"]["o2_min"]:
                         cur.execute(
-                            "INSERT INTO incubadora.incidencias (nivel_prioridad, descripcion, estado_robot) VALUES (%s, %s, %s)",
-                            ("ADVERTENCIA", f"O2 bajo: {valor}%", "OPERATIVO")
+                            "UPDATE Incubadora SET temperatura = %s, vibracion = %s, o2 = %s, co2 = %s WHERE codigo = %s; ", (temp, vibracion, o2, co2, cod_incubadora)
                         )
-
-                    # Temperatura alta
-                    elif dispositivo == "esp2" and parametro == "temperatura" and valor > CONFIG_CONTROL["esp2"]["temp_max"]:
                         cur.execute(
-                            "INSERT INTO incubadora.incidencias (nivel_prioridad, descripcion, estado_robot) VALUES (%s, %s, %s)",
-                            ("ADVERTENCIA", f"Exceso de calor: {valor}°C", "OPERATIVO")
+                            "INSERT INTO medidas(codigo_planta, codigo_incubadora, temperatura_m, vibracion_m, o2_m, co2_m) VALUES (%s, %s, %s, %s, %s, %s);", (cod_planta, cod_incubadora, temp, vibracion, o2, co2)
                         )
-
-                    # Vibración muy alta
-                    elif dispositivo == "esp3" and parametro == "vibracion" and valor > CONFIG_CONTROL["esp3"]["vibe_max"]:
-                        cur.execute(
-                            "INSERT INTO incubadora.incidencias (nivel_prioridad, descripcion, estado_robot) VALUES (%s, %s, %s)",
-                            ("ADVERTENCIA", f"Vibración base robot: {valor}G", "OPERATIVO")
-                        )
-                
+                        break
+               
                 # Guardar cambios
                 conn.commit()
-                print(f"Datos de {dispositivo} procesados correctamente.")
+                print(f"Datos procesados correctamente.")
 
     except Exception as e:
         print(f"Error procesando mensaje: {e}")
@@ -112,3 +104,5 @@ client.subscribe("incubadora/#")
 
 print("Servidor de la incubadora escuchando MQTT...")
 client.loop_forever()
+
+*/
