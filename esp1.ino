3015b1c8d01125e9aecad733924abf01750835fb
AntoniRagaMocholi <antoniwintermono@gmail.com>
2026-06-04 13:16:26 +0200

    commit

--- esp1.ino
+++ esp1.ino
@@ -2,50 +2,74 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-#define MQTT_SERVER_IP      "mqtt.dsic.upv.es"
-#define MQTT_SERVER_PORT    1883
-#define MQTT_USERNAME       "giirob"
-#define MQTT_PASSWORD       "UPV2024"
+const char* ssid = "UPV-PSK";
+const char* password = "giirob-pr2-2023";
+const char* mqtt_server = "mqtt.dsic.upv.es";
+const int mqtt_port = 1883;
+const char* mqtt_user = "giirob";
+const char* mqtt_pass = "UPV2024";
 
-#define PIN_VIBRACION 1 
-
-const char* ssid = "TU_WIFI_NOMBRE";
-const char* password = "TU_WIFI_PASSWORD";
+#define PIN_TEMP_ANALOG 4
+#define PIN_VIBRACION   5   
+#define PIN_CO2         34 
+#define PIN_O2          35  
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
   Serial.begin(115200);
+  
+  // Inicializar la red WiFi de la escuela
   setup_wifi();
-  client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
+  client.setServer(mqtt_server, mqtt_port);
 }
 
 void setup_wifi() {
+  delay(10);
+  Serial.println("\nConectando ESP1 a WiFi...");
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) { delay(500); }
+  
+  while (WiFi.status() != WL_CONNECTED) { 
+    delay(500); 
+    Serial.print(".");
+  }
+  Serial.println("\n¡WiFi Conectado en ESP1!");
 }
 
 void reconnect() {
   while (!client.connected()) {
-    if (client.connect("ESP32_Motor_S3", MQTT_USERNAME, MQTT_PASSWORD)) {
-      Serial.println("ESP3 Conectada");
-    } else { delay(5000); }
+    Serial.print("ESP1 intentando conexión MQTT...");
+    if (client.connect("ESP32_Ambiente_S1", mqtt_user, mqtt_pass)) {
+      Serial.println(" ¡Conectada con éxito al Broker!");
+    } else { 
+      Serial.print("Falló con estado ");
+      Serial.print(client.state());
+      Serial.println(" Intentando de nuevo en 5 segundos...");
+      delay(5000); 
+    }
   }
 }
 
 void loop() {
   if (!client.connected()) reconnect();
-  client.loop();
+  client.loop();  
 
-  float valorVibe = analogRead(PIN_VIBRACION) * (5.0 / 4095.0);
+  float t = analogRead(PIN_TEMP_ANALOG) * (100.0 / 4095.0);
+  float v = analogRead(PIN_VIBRACION) * (5.0 / 4095.0);
+  int valorCO2 = analogRead(PIN_CO2); 
+  float valorO2 = analogRead(PIN_O2) * (25.0 / 4095.0);
 
-  StaticJsonDocument<128> doc;
-  doc["vibracion"] = valorVibe;
+  StaticJsonDocument<256> doc;
+  doc["temperatura"] = t;
+  doc["vibracion"] = v;
+  doc["co2"] = valorCO2;
+  doc["o2"] = valorO2;
 
-  char buffer[128];
+  char buffer[256];
   serializeJson(doc, buffer);
-  client.publish("incubadora/esp3", buffer);
+  
+  client.publish("incubadora/1/101", buffer);
   
   delay(2000);
 }
